3fa39a8f08e382530f02d0d278772d19e29d0adb
omegaui <omegaui22@gmail.com>
2021-09-25 04:41:55 +0000

    Beta Image Update!

--- src/omega/jdk/JDKManager.java
+++ src/omega/jdk/JDKManager.java
@@ -67,7 +67,7 @@ public class JDKManager {
           			readRTJarFile();
                }
           }
-          catch(Exception e){ 
+          catch(Exception e){
           	e.printStackTrace();
           }
 	}
