ebe08a5852090000c762a2fa5e160f6021c82c15
omegaui <omegaui22@gmail.com>
2021-11-14 18:56:17 +0530

    Enhanced Universal FileWizard

--- src/omega/instant/support/FileWizard.java
+++ src/omega/instant/support/FileWizard.java
@@ -1,5 +1,5 @@
 /**
-The FileWizard
+* The FileWizard
 * Copyright (C) 2021 Omega UI
 
 * This program is free software: you can redistribute it and/or modify
@@ -38,50 +38,97 @@ import omega.utils.IconManager;
 
 import omega.comp.TextComp;
 import omega.comp.NoCaretField;
+import omega.comp.EdgeComp;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import static omega.utils.UIManager.*;
 import static omega.comp.Animations.*;
 public class FileWizard extends JDialog{
+	
+	private TextComp iconComp;
+	
+	
+	private NoCaretField nameField;
+	private TextComp typeComp;
+	private EdgeComp typeLabel;
+	
+	private TextComp createComp;
+	private TextComp closeComp;
+	
 	public TextComp parentRoot;
 	public TextComp typeBtn;
+	
 	public FileWizard(JFrame f){
 		super(f, true);
 		setLayout(null);
 		setUndecorated(true);
 		setSize(400, 120);
 		setLocationRelativeTo(null);
+		JPanel panel = new JPanel(null);
+		panel.setBackground(c2);
+		setContentPane(panel);
+		setLayout(null);
+		setBackground(c2);
 		setResizable(false);
 		init();
 	}
 	
 	private void init(){
-		NoCaretField nameField = new NoCaretField("", "type file name", TOOLMENU_COLOR2, c2, TOOLMENU_COLOR3);
-		nameField.setToolTipText("Enter name of the File or Source");
-		nameField.setBounds(0, 0, getWidth() - 40, 40);
-		nameField.setFont(PX16);
-		add(nameField);
-		addKeyListener(nameField);
+		iconComp = new TextComp(IconManager.fluentnewItemImage, 48, 48, back2, c2, c2, null);
+		iconComp.setBounds(5, 5, 50, 50);
+		iconComp.setArc(5, 5);
+		iconComp.setClickable(false);
+		iconComp.attachDragger(this);
+		iconComp.setEnter(true);
+		add(iconComp);
 		
 		final FileSelectionDialog fileC = new FileSelectionDialog(this);
 		fileC.setTitle("Select a directory as source root");
 		
-		parentRoot = new TextComp(":", TOOLMENU_COLOR2_SHADE, TOOLMENU_COLOR2, c2, ()->{
+		parentRoot = new TextComp(IconManager.fluentsourceImage, 20, 20, back2, back2, c2, ()->{
 			LinkedList<File> selections = fileC.selectDirectories();
 			if(!selections.isEmpty()){
 				parentRoot.setToolTipText(selections.get(0).getAbsolutePath());
 			}
 		});
-		parentRoot.setArc(0, 0);
-		parentRoot.setFont(PX16);
-		parentRoot.setBounds(nameField.getWidth(), 0, 40, 40);
+		parentRoot.setBounds(5, getHeight() - 35, 30, 30);
+		parentRoot.setArc(5, 5);
 		add(parentRoot);
+		
+		nameField = new NoCaretField("", "Start Typing", TOOLMENU_COLOR3, c2, TOOLMENU_COLOR2);
+		nameField.setBounds(65, 5, getWidth() - 100, 30);
+		nameField.setFont(PX14);
+		add(nameField);
+		addKeyListener(nameField);
+		
+		typeLabel = new EdgeComp("Type", back2, TOOLMENU_GRADIENT, glow, null);
+		typeLabel.setBounds(65, 45, 80, 25);
+		typeLabel.setFont(UBUNTU_PX14);
+		typeLabel.setLookLikeLabel(true);
+		add(typeLabel);
+		
+		createComp = new TextComp(IconManager.fluentnewfileImage, 25, 25, "Create", back2, back2, c2, this::create);
+		createComp.setBounds(getWidth() - 35, getHeight() - 35, 30, 30);
+		createComp.setArc(5, 5);
+		nameField.setOnAction(createComp.runnable);
+		add(createComp);
+		
+		closeComp = new TextComp(IconManager.fluentcloseImage, 20, 20, back2, back2, c2, this::dispose);
+		closeComp.setBounds(getWidth() - 35, 5, 30, 30);
+		closeComp.setArc(5, 5);
+		add(closeComp);
+		
+		putAnimationLayer(iconComp, getImageSizeAnimationLayer(25, -6, true), ACTION_MOUSE_PRESSED);
+		putAnimationLayer(parentRoot, getImageSizeAnimationLayer(25, 5, true), ACTION_MOUSE_ENTERED);
+		putAnimationLayer(createComp, getImageSizeAnimationLayer(25, 5, true), ACTION_MOUSE_ENTERED);
+		putAnimationLayer(closeComp, getImageSizeAnimationLayer(25, 5, true), ACTION_MOUSE_ENTERED);
 		
 		final OPopupWindow popup = new OPopupWindow("File-Type Menu", this, 0, false).width(210);
 		typeBtn = new TextComp("class", TOOLMENU_COLOR1_SHADE, c2, TOOLMENU_COLOR1, ()->{});
-		typeBtn.setBounds(0, nameField.getHeight(), getWidth(), 40);
+		typeBtn.setBounds(150, typeLabel.getY(), 120, 25);
 		popup.createItem("Directory", IconManager.projectImage, ()->typeBtn.setText("directory"))
 		.createItem("Class", IconManager.classImage, ()->typeBtn.setText("class"))
 		.createItem("Record", IconManager.recordImage, ()->typeBtn.setText("record"))
@@ -96,74 +143,63 @@ public class FileWizard extends JDialog{
 				popup.setVisible(true);
 			}
 		});
-		typeBtn.setFont(PX16);
+		typeBtn.setFont(PX14);
 		typeBtn.setArc(0, 0);
 		add(typeBtn);
-		
-		TextComp cancelBtn = new TextComp("Close", TOOLMENU_COLOR3_SHADE, c2, TOOLMENU_COLOR3, this::dispose);
-		cancelBtn.setBounds(0, getHeight() - 40, getWidth()/2, 40);
-		cancelBtn.setFont(PX16);
-		cancelBtn.setArc(0, 0);
-		add(cancelBtn);
-		
-		TextComp createBtn = new TextComp("Create", TOOLMENU_COLOR3_SHADE, c2, TOOLMENU_COLOR3, ()->{
-			if(parentRoot.getToolTipText() == null) return;
-			if(!new File(parentRoot.getToolTipText()).exists()) {
-				nameField.setText("The Root Directory Does not exists");
+	}
+	
+	public void create(){
+		if(parentRoot.getToolTipText() == null) return;
+		if(!new File(parentRoot.getToolTipText()).exists()) {
+			nameField.setText("The Root Directory Does not exists");
+			return;
+		}
+		String type = typeBtn.getText();
+		if(type.equals("directory")){
+			File dir = new File(parentRoot.getToolTipText() + File.separator + nameField.getText());
+			dir.mkdir();
+			Screen.getProjectView().reload();
+			return;
+		}
+		if(!type.equals("Custom File")){
+			String text = nameField.getText();
+			if(!text.contains(".")){
+				nameField.setText("Specify a package as pack.Class");
 				return;
 			}
-			String type = typeBtn.getText();
-			if(type.equals("directory")){
-				File dir = new File(parentRoot.getToolTipText() + File.separator + nameField.getText());
-				dir.mkdir();
-				Screen.getProjectView().reload();
-				return;
+			final String PATH = text.substring(0, text.lastIndexOf('.'));
+			String path = PATH;
+			StringTokenizer tokenizer = new StringTokenizer(path, ".");
+			path = parentRoot.getToolTipText() + File.separator;
+			while(tokenizer.hasMoreTokens()){
+				path += tokenizer.nextToken() + File.separator;
+				File file = new File(path);
+				if(!file.exists())
+					file.mkdir();
 			}
-			if(!type.equals("Custom File")){
-				String text = nameField.getText();
-				if(!text.contains(".")){
-					nameField.setText("Specify a package as pack.Class");
-					return;
+			File src = new File(path+text.substring(text.lastIndexOf('.') + 1)+".java");
+			createSRCFile(src, type, PATH, text.substring(text.lastIndexOf('.') + 1));
+		}
+		else{
+			File file = new File(parentRoot.getToolTipText() + File.separator + nameField.getText());
+			if(!file.exists()){
+				try{
+					file.createNewFile();
 				}
-				final String PATH = text.substring(0, text.lastIndexOf('.'));
-				String path = PATH;
-				StringTokenizer tokenizer = new StringTokenizer(path, ".");
-				path = parentRoot.getToolTipText() + File.separator;
-				while(tokenizer.hasMoreTokens()){
-					path += tokenizer.nextToken() + File.separator;
-					File file = new File(path);
-					if(!file.exists())
-						file.mkdir();
+				catch(Exception ex){
+					nameField.setText("Access Denied");
 				}
-				File src = new File(path+text.substring(text.lastIndexOf('.') + 1)+".java");
-				createSRCFile(src, type, PATH, text.substring(text.lastIndexOf('.') + 1));
+				try{
+					Screen.getProjectView().reload();
+					Screen.getScreen().loadFile(file);
+				}
+				catch(Exception e){
+					System.err.println(e);
+				}
 			}
-			else{
-				File file = new File(parentRoot.getToolTipText() + File.separator + nameField.getText());
-				if(!file.exists()){
-					try{
-						file.createNewFile();
-					}
-					catch(Exception ex){
-						nameField.setText("Access Denied");
-					}
-					try{
-						Screen.getProjectView().reload();
-						Screen.getScreen().loadFile(file);
-					}
-					catch(Exception e){
-						System.err.println(e);
-					}
-				}
-				else
-					nameField.setText("File Already Exists");
-			}
-		});
-		createBtn.setBounds(getWidth()/2, getHeight() - 40, getWidth()/2, 40);
-		createBtn.setFont(PX16);
-		createBtn.setArc(0, 0);
-		nameField.setOnAction(createBtn.runnable);
-		add(createBtn);
+			else
+				nameField.setText("File Already Exists");
+		}
 	}
 	
 	public void show(String type){
