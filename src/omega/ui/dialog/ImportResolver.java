36338c2bf51121a208ed78fadcb66985f94ee5de
omegaui <omegaui22@gmail.com>
2022-04-26 20:20:27 +0530

    Hit `Enter` Key to close the Import Resolver Dialog after selection!

--- src/omega/ui/dialog/ImportResolver.java
+++ src/omega/ui/dialog/ImportResolver.java
@@ -1,22 +1,24 @@
 /**
-* Auto-Import Dialog
-* Copyright (C) 2021 Omega UI
+ * Auto-Import Dialog
+ * Copyright (C) 2021 Omega UI
 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package omega.ui.dialog;
+import omegaui.listener.KeyStrokeListener;
+
 import omega.instant.support.java.management.Import;
 
 import omegaui.component.TextComp;
@@ -28,6 +30,7 @@ import java.awt.Dimension;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.KeyEvent;
 
 import java.util.LinkedList;
 
@@ -41,12 +44,15 @@ import static omega.io.UIManager.*;
 import static omegaui.component.animation.Animations.*;
 public class ImportResolver extends JDialog {
 	private JScrollPane scrollPane;
+	
 	private LinkedList<TextComp> comps = new LinkedList<>();
+	
 	private JPanel panel = new JPanel(null);
+	
 	private int pressX;
 	private int pressY;
 	private int block;
-	
+
 	public ImportResolver() {
 		super(Screen.getProjectFile().getScreen(), true);
 		setTitle("Import Resolver");
@@ -57,16 +63,23 @@ public class ImportResolver extends JDialog {
 		setLayout(null);
 		setBackground(c2);
 		setShape(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), 15, 15));
+
+		KeyStrokeListener keyListener = new KeyStrokeListener(this);
+		keyListener.putKeyStroke((e)->{
+			dispose();
+		}, KeyEvent.VK_ENTER);
+		addKeyListener(keyListener);
+		
 		init();
 	}
-	
+
 	public void init(){
 		scrollPane = new JScrollPane(panel);
 		panel.setBackground(c2);
 		scrollPane.setBounds(0, 40, getWidth(), getHeight() - 40);
 		scrollPane.setBorder(null);
 		add(scrollPane);
-		
+
 		TextComp titleComp = new TextComp("Select the imports and Click \'x\'", TOOLMENU_COLOR3_SHADE, c2, TOOLMENU_COLOR3, ()->{});
 		titleComp.setBounds(40, 0, getWidth() - 40, 40);
 		titleComp.setFont(PX16);
@@ -86,14 +99,14 @@ public class ImportResolver extends JDialog {
 			}
 		});
 		add(titleComp);
-		
+
 		TextComp closeComp = new TextComp("x", TOOLMENU_COLOR2_SHADE, c2, TOOLMENU_COLOR2, this::dispose);
 		closeComp.setBounds(0, 0, 40, 40);
 		closeComp.setFont(PX16);
 		closeComp.setArc(0, 0);
 		add(closeComp);
 	}
-	
+
 	public LinkedList<Import> resolveImports(LinkedList<Import> imports) {
 		if(imports.size() <= 1) {
 			imports.clear();
@@ -123,14 +136,14 @@ public class ImportResolver extends JDialog {
 			block += 30;
 		}
 		panel.setPreferredSize(new Dimension(maxW, block));
-		
+
 		setVisible(true);
-		
+
 		comps.forEach(comp->{
 			if(comp.color2 == TOOLMENU_COLOR3)
 				selections.add(new Import(comp.getText(), "", false));
 		});
-		
+
 		comps.forEach(panel::remove);
 		comps.clear();
 		repaint();
